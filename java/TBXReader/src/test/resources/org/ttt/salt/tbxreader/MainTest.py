0fa182837b6a3727353b6f89d533fb9e095e9da7
Lance Finn Helsten <devnull@localhost>
2017-11-08 07:45:36 -0700

    Change license

--- java/TBXReader/src/test/resources/org/ttt/salt/tbxreader/MainTest.py
+++ java/TBXReader/src/test/resources/org/ttt/salt/tbxreader/MainTest.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python
-# $Id: MainTest.py 1 2008-05-23 03:51:58Z lanhel $
 #-----------------------------------------------------------------------------
-# Copyright (C) 1997-2000 Lance Finn Helsten (helsten@acm.org)
-#
-# This library is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the
-# Free Software Foundation; either version 2 of the License, or (at your
-# option) any later version.
-#
-# This library is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-# for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this library; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+__author__ = ('Lance Finn Helsten',)
+__version__ = '1.0.2'
+__copyright__ = """Copyright 1997-2011 Lance Finn Helsten (helsten@acm.org)"""
+__license__ = """
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 
 from sys import *
 import os
